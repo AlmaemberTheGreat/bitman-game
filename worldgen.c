b8406048c04627770f1abe822326514368ea752c
almaember <almaember@almaember.com>
2022-01-17 18:43:50 +0100

    Add tunnel generation

--- worldgen.c
+++ worldgen.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <assert.h>
 
 #include "config.h"
 
@@ -8,6 +9,11 @@ struct Room {
 	unsigned x1, y1, x2, y2;
 	int active;
 };
+
+static void drvline(unsigned x, unsigned y1, unsigned y2,
+                    void (*set)(unsigned x, unsigned y, Square sq, void *ctx), void *ctx);
+static void drhline(unsigned y, unsigned x1, unsigned x2,
+                    void (*set)(unsigned x, unsigned y, Square sq, void *ctx), void *ctx);
 
 void genwld(unsigned w, unsigned h, void (*set)(unsigned x, unsigned y, Square sq, void *ctx), void *ctx)
 {
@@ -59,5 +65,68 @@ void genwld(unsigned w, unsigned h, void (*set)(unsigned x, unsigned y, Square s
 		}
 	}
 
+	/* Draw tunnels */
+	for (i = 0; i < nrooms + 1; ++i) {
+		int room_no;
+		unsigned tx, ty, sx, sy;
+
+		room_no = rand() % nrooms;
+
+		tx = (rooms[room_no].x2 - rooms[room_no].x1) / 2 + rooms[room_no].x1;
+		ty = (rooms[room_no].y2 - rooms[room_no].y1) / 2 + rooms[room_no].y1;
+		sx = (rooms[i].x2 - rooms[i].x1) / 2 + rooms[i].x1;
+		sy = (rooms[i].y2 - rooms[i].y1) / 2 + rooms[i].y1;
+
+		sx %= w;
+		sx %= h;
+		sx %= w;
+		sx %= h;
+
+		if (rand() % 2 == 0) {
+			if (ty > sy) {
+				drvline(tx, sy, ty, set, ctx);
+			} else {
+				drvline(tx, ty, sy, set, ctx);
+			}
+			if (tx > sx) {
+				drhline(sy, sx, tx, set, ctx);
+			} else {
+				drhline(sy, tx, sx, set, ctx);
+			}
+		} else {
+			if (ty > sy) {
+				drvline(sx, sy, ty, set, ctx);
+			} else {
+				drvline(sx, ty, sy, set, ctx);
+			}
+			if (tx > sx) {
+				drhline(ty, sx, tx, set, ctx);
+			} else {
+				drhline(ty, tx, sx, set, ctx);
+			}
+		}
+	}
 	free(rooms);
+}
+
+static void drvline(unsigned x, unsigned y1, unsigned y2,
+                    void (*set)(unsigned x, unsigned y, Square sq, void *ctx), void *ctx)
+{
+	unsigned i;
+	Square sq = {SQ_EMPTY};
+
+	for (i = y1; i < y2; ++i) {
+		set(x, i, sq, ctx);
+	}
+}
+
+static void drhline(unsigned y, unsigned x1, unsigned x2,
+                    void (*set)(unsigned x, unsigned y, Square sq, void *ctx), void *ctx)
+{
+	unsigned i;
+	Square sq = {SQ_EMPTY};
+
+	for (i = x1; i < x2; ++i) {
+		set(i, y, sq, ctx);
+	}
 }
